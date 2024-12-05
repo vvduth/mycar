d4f6b109195ef1dd39c9c5414e327474fb6c2a18
vvduth <ducthai060501@gmail.com>
2024-12-05 13:35:59 +0200

    created an end to end test

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,6 @@ const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieSession({
-    keys: ['random']
-  }));
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    })
-  )
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
