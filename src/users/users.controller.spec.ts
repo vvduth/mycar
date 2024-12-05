89ea2dcfa1520fc0b85f4c087d75907ce0a35224
vvduth <ducthai060501@gmail.com>
2024-12-05 13:05:05 +0200

    added unit test controller (really not useful and basic)

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,12 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
+import { User } from './user.entity';
+import { NotFoundException } from '@nestjs/common';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let fakeUsersService: Partial<UsersService>
+  let fakeAuthService: Partial<AuthService> 
 
   beforeEach(async () => {
+
+    fakeUsersService ={
+      findOne: (id: number) => {return Promise.resolve({
+        id:1,
+        email: 'ducops@email.com', 
+        password: '2312321313'
+      } as User)},
+      find: (email: string) => {
+        return Promise.resolve([{
+          id: 1, email, password: '2312321313'
+        } as User])
+      },
+      // remove: () => {},
+      // update: () => {},
+    }
+    fakeAuthService = {
+      // signup: (email, password) => {},
+       signin: (email:string, password: string ) => {
+        return Promise.resolve({id: 1, email , password} as User)
+       }
+    }
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers : [
+        {provide: UsersService, useValue: fakeUsersService },
+        {provide: AuthService, useValue: fakeAuthService}
+      ]
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
@@ -15,4 +46,30 @@ describe('UsersController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('findAllUser rreturn a list of users with given email', async ( ) => {
+    const users = await controller.findAllUser('ducops@email.com')
+    expect(users.length === 1)
+    expect(users[0].email === 'ducops@email.com')
+  })
+
+  it ('test findUser returns null if user with given id is not found', async () => {
+    fakeUsersService.findOne = ( ) => null;
+    await expect(controller.getUser('1')).toEqual(null)
+  })
+
+  it('findUser return a single user with the given id', async () => {
+    const user = await controller.getUser('1')
+    expect(user).toBeDefined()
+  })
+
+  it('singIn update session obejct and return user', async () => {
+    const session = {userId: -10}
+    const user = await controller.signin({email: 'ducops@4real.com', password: '12121313'},
+      session
+    )
+
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1)
+  })
 });
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,14 +13,12 @@ import {
 import { CreateUserDTO } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDTO } from './dtos/update-user.dto';
-import {
-  Serialize,
-} from 'src/interceptors/serialize.interceptor';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { User } from './user.entity';
-import { AuthGuard } from 'src/guards/auth.guard';
+import { AuthGuard } from '../guards/auth.guard';
 @Controller('auth')
 @Serialize(UserDto)
 
