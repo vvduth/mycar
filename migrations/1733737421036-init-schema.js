425044febdf0999d4e4d8afe3638f49a3ee7cda7
vvduth <ducthai060501@gmail.com>
2024-12-09 11:44:19 +0200

    fix migration problem with database

---
+++ migrations/1733737421036-init-schema.js
@@ -0,0 +1,11 @@
+const { MigrationInterface, QueryRunner } = require("typeorm");
+
+module.exports = class InitSchema1733737421036 {
+
+    async up(queryRunner) {
+    }
+
+    async down(queryRunner) {
+    }
+
+}
